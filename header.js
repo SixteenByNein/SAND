46e7f6a99a74a170cf90d9fdf91c78a7ac9a055f
SixteenByNein <80447714+SixteenByNein@users.noreply.github.com>
2023-03-28 11:51:44 -0700

    Initial commit

---
+++ header.js
@@ -0,0 +1,9 @@
+function header()	{
+	
+	const header = document.createElement("header");
+	
+	header.innerHTML = "<div id = 'universalHeader'> <h1 id = 'topLogo'>Sand</h1><h4>A Genre-Agnostic TTRPG</h4><div id = 'options'><div class = 'headerOption'><a href = 'rules/rulesHomepage.html'>Rules</a></div><div class = 'headerOption'><a href = 'playersPage/Player.html'>For Players</a></div><div class = 'headerOption'><a href = 'GMsPage/GM.html'>For Game Masters</a></div><div class = 'headerOption'><a href = 'modules/modules.html'>Modules</a></div></div>"
+	
+	document.body.appendChild(header);
+	
+}
