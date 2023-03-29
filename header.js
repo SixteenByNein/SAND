50d75e74ddf05e49e4558410abe8892492afff15
SixteenByNein <80447714+SixteenByNein@users.noreply.github.com>
2023-03-29 16:24:29 -0700

    More experimentation

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ function header()	{
 	
 	const header = document.createElement("header");
 	
-	header.innerHTML = "<div id = 'universalHeader'> <a href = '/index.html'><h1 id = 'topLogo'>Sand</h1></a><h4>A Genre-Agnostic TTRPG</h4><div id = 'options'><div class = 'headerOption'><a href = '/rules/rulesHomepage.html'>Rules</a></div><div class = 'headerOption'><a href = '/playersPage/Player'>For Players</a></div><div class = 'headerOption'><a href = '/GMsPage/GM.html'>For Game Masters</a></div><div class = 'headerOption'><a href = '/modules/modules.html'>Modules</a></div></div>"
+	header.innerHTML = "<div id = 'universalHeader'> <a href = '/index.html'><h1 id = 'topLogo'>Sand</h1></a><h4>A Genre-Agnostic TTRPG</h4><div id = 'options'><div class = 'headerOption'><a href = '/rules/rulesHomepage.html'>Rules</a></div><div class = 'headerOption'><a href = '/playersPage/player'>For Players</a></div><div class = 'headerOption'><a href = '/GMsPage/GM.html'>For Game Masters</a></div><div class = 'headerOption'><a href = '/modules/modules.html'>Modules</a></div></div>"
 	
 	document.body.appendChild(header);
 	
