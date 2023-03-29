96b9379a56027ccc8da2b144454b7863968d4f9a
SixteenByNein <80447714+SixteenByNein@users.noreply.github.com>
2023-03-29 16:51:57 -0700

    Update header.js

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ function header()	{
 	
 	const header = document.createElement("header");
 	
-	header.innerHTML = "<div id = 'universalHeader'> <a href = '/index.html'><h1 id = 'topLogo'>Sand</h1></a><h4>A Genre-Agnostic TTRPG</h4><div id = 'options'><a href = '/rules/rulesHomepage.html'><div class = 'headerOption'>Rules</div></a><div class = 'headerOption'><a href = '/playersPage/player'>For Players</a></div><div class = 'headerOption'><a href = '/GMsPage/GM.html'>For Game Masters</a></div><div class = 'headerOption'><a href = '/modules/modules.html'>Modules</a></div></div>"
+	header.innerHTML = "<div id = 'universalHeader'> <a href = '/index.html'><h1 id = 'topLogo'>Sand</h1></a><h4>A Genre-Agnostic TTRPG</h4><div id = 'options'><a href = '/rules/rulesHomepage.html'><div class = 'headerOption'>Rules</div></a><a href = '/playersPage/player'><div class = 'headerOption'>For Players</div></a><a href = '/GMsPage/GM.html'><div class = 'headerOption'>For Game Masters</div></a><a href = '/modules/modules.html'><div class = 'headerOption'>Modules</div></a></div>"
 	
 	document.body.appendChild(header);
 	
